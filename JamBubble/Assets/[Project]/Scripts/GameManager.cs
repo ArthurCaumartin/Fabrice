bda5cee746ad789a6c1260964a4966b05ee5b02f
ozerlodev <ozerlo.games@gmail.com>
2025-01-23 14:57:26 +0100

    SKIN

--- JamBubble/Assets/[Project]/Scripts/GameManager.cs
+++ JamBubble/Assets/[Project]/Scripts/GameManager.cs
@@ -54,6 +54,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TMP_Text teamNameText;
     [SerializeField] CinemachineTargetGroup targetCam;
     [SerializeField] private CameraShake camShake;
+    [SerializeField] private GameObject arbitre;
+
 
 
     private void Awake(){
@@ -78,8 +80,11 @@ public class GameManager : MonoBehaviour
             }
 
             SetTimerText();
-            
         }
+        Vector3 targetPostition = new Vector3( ball.position.x, 
+                                       arbitre.transform.position.y, 
+                                       ball.position.z ) ;
+        arbitre.transform.LookAt( targetPostition ) ;
     }
 
     private void SetTimerText(){
@@ -172,6 +177,9 @@ public class GameManager : MonoBehaviour
     public void AddPoint(Team teamPoint){
         Shake(1.5f, 1f);
         AudioManager.Instance.PlaySFX("goal", .5f);
+        arbitre.transform.GetChild(0).DOLocalMoveY(.3f, .5f).From(0.125f).SetEase(Ease.OutBounce).SetInverted();
+        arbitre.transform.GetChild(0).DOLocalRotate(new Vector3(-0.25f,0,0), .5f).From(Vector3.zero).SetEase(Ease.OutBounce).SetInverted();
+
         ControllerVibrationEveryone(.5f,1.5f);
         goalCanvas.SetActive(true);
 
