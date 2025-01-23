d153de85b4f78307b0c23cb77fdcbb72e2fbad53
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-01-23 13:06:52 +0100

    bullet mesh

--- JamBubble/Assets/[Project]/Scripts/AutoScaleOnCollider.cs
+++ JamBubble/Assets/[Project]/Scripts/AutoScaleOnCollider.cs
@@ -6,10 +6,11 @@ using UnityEngine;
 public class AutoScaleOnCollider : MonoBehaviour
 {
     [SerializeField] private SphereCollider sphereCollider;
+    [SerializeField] private float _scale = 1;
 
     void Update()
     {
         if(!sphereCollider) return;
-        transform.localScale = Vector3.one * sphereCollider.radius * 2;
+        transform.localScale = Vector3.one * sphereCollider.radius * 2 * _scale;
     }
 }
