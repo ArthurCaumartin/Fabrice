6963e59e68f3faf534cea0253b95e9c8211e35c9
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-01-23 15:03:15 +0100

    aim line

---
+++ JamBubble/Assets/[Project]/Scripts/LineFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
+
+public class LineFollow : MonoBehaviour
+{
+    [SerializeField] private float _lenght = 5;
+    [SerializeField, Range(0, 1)] private float _inputMag = 1;
+    private LineRenderer _line;
+    private Player _player;
+
+    private void Start()
+    {
+        _player = transform.parent.GetComponent<PlayerManager>().player; 
+        _line = GetComponent<LineRenderer>();
+    }
+
+    private void OnValidate()
+    {
+        _line = GetComponent<LineRenderer>();
+        UpdatePos();
+    }
+
+    public void Update()
+    {
+        if (!_line || _player == null) Start();
+        _inputMag = new Vector2(_player.GetAxis("Horizontal"), _player.GetAxis("Vertical")).magnitude;
+        UpdatePos();
+    }
+
+    private void UpdatePos()
+    {
+        _line.SetPosition(0, transform.position);
+        _line.SetPosition(1, transform.position + (transform.forward * _lenght * _inputMag));
+    }
+}
